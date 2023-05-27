ff9e501eb2c2c2d545b17f08cb063eb047f1c363
LigatCindra <128463690+LigatCindra@users.noreply.github.com>
2023-05-27 19:33:15 +0700

    update
    bagian kerja

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-// import "./App.css";
+import "./App.css";
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import CharacterList from "./Components/Character/CharacterList";
 import ComicList from "./Components/Comic/ComicList";
 import EventList from "./Components/Event/EventList";
@@ -8,10 +9,35 @@ import SeriesList from "./Components/Series/SeriesList";
 function App() {
   return (
     <div className="App">
-      {/* <CharacterList /> */}
-      {/* <ComicList /> */}
-      {/* <EventList /> */}
-      <SeriesList />
+      <BrowserRouter>
+        <nav>
+          <ul>
+          <h1>Marvel Wiki</h1>
+            <li>
+              <Link to="/">Character</Link>
+            </li>
+            <li>
+              <Link to="/comics">Comics</Link>
+            </li>
+            <li>
+              <Link to="/events">Events</Link>
+            </li>
+            <li>
+              <Link to="/series">Series</Link>
+            </li>
+            <li>
+              <Link to="/aboutus">About Us</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<CharacterList />} />
+          <Route path="/comics" element={<ComicList />} />
+          <Route path="/events" element={<EventList />} />
+          <Route path="/series" element={<SeriesList />} />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
