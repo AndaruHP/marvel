09b990af2d824ed7ad19d85f3677b49bb76618f7
Andaru Hymawan <130301294+AndaruHP@users.noreply.github.com>
2023-05-30 20:40:17 +0700

    Update Homepage.js

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -8,16 +8,12 @@ function Homepage() {
       <div className="homepage-container">
         <h1>MARVEL WIKI</h1>
         <p className="homepage-container-p">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          Welcome to Marvel Wiki! <br /> Explore the extraordinary world of the
+          Marvel Universe with us. <br /> Begin your adventure by pressing the
+          button below.
         </p>
         <Link to="/characters">
-          <button>Masuk ke laman utama</button>
+          <button>"Unleash the Marvel Magic!"</button>
         </Link>
       </div>
     </div>
